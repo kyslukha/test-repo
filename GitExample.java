11a18d1307cf1fe04fdc0e25ef8fbf7f8c3d123e
kyslukha <kyslukha@gmail.com>
2020-12-07 19:40:57 +0200

    my first commit

---
+++ GitExample.java
@@ -0,0 +1,10 @@
+package com.alevel.lesson3;
+
+import java.util.Arrays;
+
+public class GitExample {
+    public static void main(String[] args) {
+        System.out.println("Hello github");
+
+    }
+}
